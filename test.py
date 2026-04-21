56e4bbe12624267e880f30eecb9193f21909d504
Mehdi-h-m <nm_hamai@esi.dz>
2026-04-21 18:14:24 +0100

    Creating the apps

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 import requests
 
 def get_meal_categories():
-    url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
+    url = "https://www.themealdb.com/api/json/v1/1/random.php"
     response = requests.get(url)
 
     if response.status_code == 200:
+     
         data = response.json()
-        categories = data.get("meals", [])
-        print("Meal Categories:")
-        for item in categories:
-            print("-", item["strCategory"])
+        print(data)    
+        # categories = data.get("meals", [])
+        # print("Meal Categories:")
+        # for item in categories:
+        #     print(item["strIngredient"])
     else:
         print("Failed to retrieve data. Status code:", response.status_code)
 
+
 if __name__ == "__main__":
     get_meal_categories()
+
