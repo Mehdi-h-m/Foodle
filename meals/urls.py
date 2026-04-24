5a91161062fbcc1482832c63bd5744db7157a51b
Mehdi-h-m <nm_hamai@esi.dz>
2026-04-24 14:51:19 +0100

    Added Tests for authentification logic

--- meals/urls.py
+++ meals/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path,include
 from meals import views
 urlpatterns = [
-    path('Discover/', views.Discover),
+    path('Discover/', views.Discover, name="Discover"),
 ]
